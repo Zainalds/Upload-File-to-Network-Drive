5410b6661c97605e4aff5b5964d424fcb20ac1e7
Zain Nabi <68240955+Zainalds@users.noreply.github.com>
2021-08-04 15:35:55 +0200

    Update FileUpload.cs

--- FileUpload.cs
+++ FileUpload.cs
@@ -1,26 +1,26 @@
-public async Task<IActionResult> UploadTarget(IFormFile File, string targetType, int FinancialYear, string redirectUrl)
+        public async Task<IActionResult> UploadTarget(IFormFile File, string targetType, int FinancialYear, string redirectUrl)
         {
             var UploadDocumentHelper = new UploadDocumentHepler();
             var UploadViewModel = new UploadViewModel();
             //var result = await UploadDocumentHelper.UploadRepTarget(File, targetType);
             var fileExt = System.IO.Path.GetExtension(File.FileName).ToLower();
 
-            var pathAndFile = "\\\\tiger\\CreditNote\\";
-            pathAndFile += File.FileName;
             if (fileExt != ".csv")
             {
                 UploadViewModel.FileErrorMessage = "Please upload a csv format file";
             }
             else
             {
+                var pathAndFile = "\\\\tiger\\CreditNote\\";
+                pathAndFile += File.FileName;                
+                var path = "\\\\tiger\\CreditNote\\";
                 if (System.IO.File.Exists(pathAndFile))
                 {
                     System.IO.File.Delete(pathAndFile);
                 }
-                using (var localFile = System.IO.File.OpenWrite(File.FileName))
-                using (var stream = System.IO.File.Create(pathAndFile))
+                using (var fileStream = new FileStream(Path.Combine(path, File.FileName), FileMode.Create))
                 {
-                    await File.CopyToAsync(localFile);
+                    await File.CopyToAsync(fileStream);
                 }
             }
             UploadViewModel.url = redirectUrl;            
